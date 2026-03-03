1f2f7417e935f34e8b64f68a6044aaa15eb62317
MUKKU RAGHAVENDRA <162527757+RAGHU1242@users.noreply.github.com>
2026-03-03 13:19:09 +0530

    Update config.js

--- agrigo/mobile/src/utils/config.js
+++ agrigo/mobile/src/utils/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://10.0.2.2:5000/api';
-export const SOCKET_URL = 'http://10.0.2.2:5000';
+export const API_BASE_URL = 'https://atbs.onrender.com/api';
+export const SOCKET_URL = 'https://atbs.onrender.com';
